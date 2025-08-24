82c790cbd0c1023ece9116e7ccc59338cb0a51a8
PaulaCab <paula.cab.car@gmail.com>
2025-08-24 12:40:17 +0200

    [Add] Init project

---
+++ devtree/index.js
@@ -0,0 +1,17 @@
+﻿import express from 'express'
+
+const app = express()
+
+//Routing
+app.get('/', (req, res)=>{
+    res.send('Hello world!')
+})
+
+app.get('/miau', (req, res)=>{
+    res.send('Miau!')
+})
+
+const port = process.env.Port || 4000
+app.listen(port, () => {
+    console.log('Server working: port ', port)
+})
